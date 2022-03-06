e230ab2f3cb8944aecbff062b9566ee72477e2d9
Nick <nicholasconnors@hotmail.ca>
2022-03-05 21:32:30 -0500

    Merge branch 'main' into Main-+-Pause-Menu-(Steven)

---
+++ Assets/Scripts/Minigame/Holdable2D.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Holdable2D : Hoverable2D
+{
+    private bool _isHeld;
+    public bool IsHeld { get { return _isHeld; } }
+    private Plane _plane;
+
+    void Update()
+    {
+        var isHovering = IsCursorHovering();
+
+        if(InputManager.IsCommandJustPressed(InputManager.InputCommand.PickUp) && isHovering)
+        {
+            _isHeld = true;
+        }
+
+        if(_isHeld)
+        {
+            if(InputManager.IsCommandJustReleased(InputManager.InputCommand.PickUp))
+            {
+                _isHeld = false;
+            }
+
+            Ray ray = Camera.main.ScreenPointToRay(InputManager.GetCursorPosition());
+
+            _plane = new Plane(Camera.main.transform.forward, transform.parent.position);
+            _plane.Raycast(ray, out float dist);
+
+            var pos = ray.GetPoint(dist);
+
+            transform.position = pos;
+        }
+    }
+
+    public override void OnStartHover()
+    {
+        EventManager<InputManager.InputCommand>.TriggerEvent("PromptHit", InputManager.InputCommand.PickUp);
+    }
+
+    public override void OnStopHover()
+    {
+        EventManager<InputManager.InputCommand>.TriggerEvent("PromptLost", InputManager.InputCommand.PickUp);
+    }
+}
