5a20971c69d21f744032de301613a1c7f38699b8
Nick <nicholasconnors@hotmail.ca>
2022-03-13 12:08:45 -0400

    Fix PR

--- Assets/Scripts/UI/MenuScripts/OptionsMenu.cs
+++ Assets/Scripts/UI/MenuScripts/OptionsMenu.cs
@@ -5,7 +5,8 @@ using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class OptionsMenu : MonoBehaviour{
+public class OptionsMenu : MonoBehaviour
+{
     public static bool GamePaused = false;
     public GameObject pauseMenuUI;
     public AudioMixer audioMixer;
@@ -16,89 +17,108 @@ public class OptionsMenu : MonoBehaviour{
     public Slider sensSlider;
     private InputManager.InputMode _lastInputMode = InputManager.InputMode.Player;
 
-    void Start(){
-       // QualitySettings.SetQualityLevel(3);
-     
-      
+    void Start()
+    {
+        // QualitySettings.SetQualityLevel(3);
+
+
         //Get array of resolutions
-        if (resolutionDropdown != null){
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-        List<string> options = new List<string>();
-        for (int i =0; i < resolutions.Length; i ++){
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+        if (resolutionDropdown != null)
+        {
+            resolutions = Screen.resolutions;
+            resolutionDropdown.ClearOptions();
+            List<string> options = new List<string>();
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
-                currentResolutionIndex = i;
+                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = i;
+                }
             }
-        }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
 
-          if (sensSlider != null){
-            sensSlider.value = (float) (playercharacter.GetComponent<CameraController>().sensX);
-       }
+            if (sensSlider != null)
+            {
+                sensSlider.value = (float)(Player.Instance.sensX);
+            }
         }
 
     }
-    public void SetVolume(float volume){
+    public void SetVolume(float volume)
+    {
         //Sets the game volume
         //Used in the slider
         audioMixer.SetFloat("volume", volume);
     }
-    public void SetMouseSensitiity(float sens){
+    public void SetMouseSensitiity(float sens)
+    {
         Player.Instance.sensX = sens;
         Player.Instance.sensY = sens;
-    }  
+    }
 
-    public void SetQuality(int qualityIndex){
+    public void SetQuality(int qualityIndex)
+    {
         //Pass in int that corresponds to a qualtiy level in Unity
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
-    public void SetFullscreen(bool isFullscreen){
+    public void SetFullscreen(bool isFullscreen)
+    {
         Screen.fullScreen = isFullscreen;
     }
 
-    public void SetResolution(int resolutionIndex){
+    public void SetResolution(int resolutionIndex)
+    {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
-     void Update() {
-         
+    void Update()
+    {
+
         //Checks if the game is paused or not
-        if (Input.GetKeyDown(KeyCode.Escape)){
-            if (GamePaused){
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
                 Resume();
-            
-            }else{
+
+            }
+            else
+            {
                 Pause();
             }
         }
     }
 
-    public void Resume(){
+    public void Resume()
+    {
         pauseMenuUI.SetActive(false);
         InputManager.CurrentInputMode = _lastInputMode;
         GamePaused = false;
     }
 
-    void Pause(){
+    void Pause()
+    {
         pauseMenuUI.SetActive(true);
         _lastInputMode = InputManager.CurrentInputMode;
         InputManager.CurrentInputMode = InputManager.InputMode.UI;
         GamePaused = true;
     }
 
-    public void LoadMenu(){
-        
+    public void LoadMenu()
+    {
+
     }
 
-    public void ReturnToMainMenu(){
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 
     }
 }
