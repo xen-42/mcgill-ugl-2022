205dcdd6390f35e88da5ff4fd8df1d415b6f952e
Daphne Ducharme <62679756+daphneducharmee@users.noreply.github.com>
2022-04-01 00:12:12 -0400

    Tutorial warning in lobby menu

--- Assets/Scripts/UI/MenuScripts/Tutorial.cs
+++ Assets/Scripts/UI/MenuScripts/Tutorial.cs
@@ -6,7 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class Tutorial : MonoBehaviour
 {
+    public GameObject tutorialUI;
+
     public void BacktoMainMenu(){
         SceneManager.LoadScene(Scenes.Lobby);
+    }
+
+    public void BacktoGame(){
+        tutorialUI.SetActive(false);
+        InputManager.CurrentInputMode = InputManager.InputMode.Player;
     }
 }
