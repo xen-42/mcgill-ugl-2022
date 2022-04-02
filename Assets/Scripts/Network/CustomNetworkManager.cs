63085deb4ce48a8497d83816dc8a3eff1fd0cf00
Nick <nicholasconnors@hotmail.ca>
2022-04-01 21:54:27 -0400

    Fix bug

--- Assets/Scripts/Network/CustomNetworkManager.cs
+++ Assets/Scripts/Network/CustomNetworkManager.cs
@@ -325,5 +325,6 @@ public class CustomNetworkManager : NetworkManager
     {
         NetworkClient.RegisterPrefab(lobbyPlayerPrefab.gameObject);
         NetworkClient.RegisterPrefab(gamePlayerPrefab.gameObject);
+        NetworkClient.RegisterPrefab(playerSpawnerPrefab.gameObject);
     }
 }
