7e3bc27409c697dcb238d971b2e6747b7b0fa260
Nick <nicholasconnors@hotmail.ca>
2022-04-06 23:05:22 -0400

    Put in player models + animations

--- Assets/Scripts/Gameplay/Player.cs
+++ Assets/Scripts/Gameplay/Player.cs
@@ -52,7 +52,7 @@ public class Player : NetworkBehaviour
     [SerializeField] private float heldItemRotationResponsiveness = 10f;
 
     [Header("Physics Stuff")]
-    [SerializeField] private Rigidbody rb;
+    [SerializeField] public Rigidbody rb;
     [SerializeField] public new Collider collider;
 
     private GameObject _focusedObject;
@@ -61,9 +61,9 @@ public class Player : NetworkBehaviour
     public float stressModifier;
 
     // Synced network stuff
-    [SyncVar] private Vector3 _movement;
-    [SyncVar] private bool _jump;
-    [SyncVar] private bool _sprint;
+    [SyncVar] public Vector3 movement;
+    [SyncVar] public bool jump;
+    [SyncVar] public bool sprint;
     [SyncVar] private float _serverSideStressModifier;
 
     public static Player Instance { get; private set; }
@@ -148,11 +148,11 @@ public class Player : NetworkBehaviour
         if (InputManager.CurrentInputMode != InputManager.InputMode.Player)
         {
             // Don't move during minigame
-            if (_movement != Vector3.zero)
+            if (this.movement != Vector3.zero)
             {
                 if (isServer)
                 {
-                    _movement = Vector3.zero;
+                    this.movement = Vector3.zero;
                 }
                 else
                 {
@@ -283,18 +283,18 @@ public class Player : NetworkBehaviour
         PlayerDrag();
         ControlSpeed();
 
-        if (_jump && isGrounded)
+        if (jump && isGrounded)
         {
             rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
             rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-            _jump = false;
+            jump = false;
         }
 
-        var slopeMoveDirection = Vector3.ProjectOnPlane(_movement, slopeHit.normal);
+        var slopeMoveDirection = Vector3.ProjectOnPlane(movement, slopeHit.normal);
 
         if (isGrounded && !OnSlope())
         {
-            rb.AddForce(_movement.normalized * actualMoveSpeed * movementMultiplier, ForceMode.Acceleration);
+            rb.AddForce(movement.normalized * actualMoveSpeed * movementMultiplier, ForceMode.Acceleration);
         }
         else if (isGrounded && OnSlope())
         {
@@ -302,7 +302,7 @@ public class Player : NetworkBehaviour
         }
         else if (!isGrounded)
         {
-            rb.AddForce(_movement.normalized * actualMoveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+            rb.AddForce(movement.normalized * actualMoveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
         }
     }
 
@@ -313,7 +313,7 @@ public class Player : NetworkBehaviour
         var actualRunSpeed = Mathf.Lerp(runSpeed, 1, _serverSideStressModifier * _serverSideStressModifier);
         var actualAcceleration = Mathf.Lerp(acceleration, 1, _serverSideStressModifier * _serverSideStressModifier);
 
-        if (_sprint && isGrounded)
+        if (sprint && isGrounded)
         {
             moveSpeed = Mathf.Lerp(actualMoveSpeed, actualRunSpeed, actualAcceleration * Time.deltaTime);
         }
@@ -322,7 +322,7 @@ public class Player : NetworkBehaviour
             moveSpeed = Mathf.Lerp(actualMoveSpeed, actualWalkSpeed, actualAcceleration * Time.deltaTime);
         }
 
-        if (_sprint && isGrounded && _movement != Vector3.zero)
+        if (sprint && isGrounded && movement != Vector3.zero)
         {
             cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fastfov, fovaccel * Time.deltaTime);
         }
@@ -373,15 +373,18 @@ public class Player : NetworkBehaviour
     [ClientRpc]
     private void RpcSendInputs(Vector3 movement, bool jump, bool sprint, float xRot, float yRot, float stress)
     {
-        _movement = movement;
-        _jump = jump;
-        _sprint = sprint;
+        this.movement = movement;
+        this.jump = jump;
+        this.sprint = sprint;
         _serverSideStressModifier = stress;
 
         if (!hasAuthority)
         {
+            // We're rotating the other character
             cam.transform.localRotation = Quaternion.Euler(xRot, yRot, 0);
             orientation.transform.rotation = Quaternion.Euler(0, yRot, 0);
+            yRotation = yRot;
+            xRotation = xRot;
         }
     }
 
@@ -455,7 +458,7 @@ public class Player : NetworkBehaviour
     [Command]
     public void CmdStopMoving()
     {
-        _movement = Vector3.zero;
+        movement = Vector3.zero;
     }
 
     #endregion Commands and RPC
